d70aeacf2485b53574a6fd9faaf66d06ef5e48e1
Ricardo Munduruca <ricardo.munduruca@gmail.com>
2020-11-26 19:57:34 +1100

    Tweaking models
    creating new object into my model

--- test_project_2/demo/models.py
+++ test_project_2/demo/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=36)
+    title = models.CharField(max_length=36, unique=True)
+    description = models.TextField(max_length=256, 
+                                   default=None)
