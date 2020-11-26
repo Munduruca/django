e03a0a619d64025726174effd2166cd7a9ea1b3e
Ricardo Munduruca <ricardo.munduruca@gmail.com>
2020-11-26 21:16:18 +1100

    Add results from the db
    adding to my views.py results from my models

--- test_project_2/demo/views.py
+++ test_project_2/demo/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
 
 class AnotherClass(View):
+
+    books = Book.objects.get(id=2)
+    #for i in books:
+    output = books.title
+
     def get(self, request):
-        return HttpResponse('My message from the class')
-
-
+        return HttpResponse(self.output)
 
 def first(request):
     return HttpResponse('First message from views')
