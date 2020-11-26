45e06cc89e1d0e7c502330adf5747f5a3873dcf5
Ricardo Munduruca <ricardo.munduruca@gmail.com>
2020-11-26 20:49:26 +1100

    Add mode views

--- test_project_2/demo/urls.py
+++ test_project_2/demo/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import AnotherClass
+
 
 urlpatterns = [
-    path('', views.first),
+    path('first', views.first),
+    path('second', AnotherClass.as_view()),
 ]
--- test_project_2/demo/views.py
+++ test_project_2/demo/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
+class AnotherClass(View):
+    def get(self, request):
+        return HttpResponse('My message from the class')
+
+
 
 def first(request):
     return HttpResponse('First message from views')
